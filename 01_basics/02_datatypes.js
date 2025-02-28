2f03dd53840d39290dd48fb2193b632d87d3bf5a
Ambike-jaiswal-1234 <thyselfseeking@gmail.com>
2025-02-28 02:19:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all js code as never version
+//alert(3+3)//we are using nodejs, not browser
+console.log(3+3)
+console.log("Radhakrishan")
+let name="ambike"
+let age="18"
+let isloggedin =false
+let state;
+
+//number 2 to power of 53
+//bigint
+//string =>""
+//boolean => true/false
+// null =>standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof null);// object
+console.log(typeof undefined);//undefind
