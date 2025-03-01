265f7f34e70aed9e5668405d30f973ac96994a67
Ambike-jaiswal-1234 <thyselfseeking@gmail.com>
2025-03-01 13:37:12 +0000

    operatorconversion

---
+++ 01_basics/03_operatorconversion.js
@@ -0,0 +1,21 @@
+
+
+//console.log(valueInNumberse)
+//
+//"33" =>33
+//"3abc3" => NaN
+//true =>1; false => 0
+
+//let isLoggedIn = "RADHA"
+
+//let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//1 - TRUE; 0- false
+//"" - false
+//"RADHA"- true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
